0884255226e7b2a9c913647d3ce097f6354214ed
AnnaKrasnitski <53522877+AnnaKrasnitski@users.noreply.github.com>
2019-07-31 15:30:39 +0300

    Add files via upload

---
+++ aps_webcomponent.js
@@ -0,0 +1,59 @@
+
+(function()  {
+let tmpl = document.createElement('template');
+Access-Control-Allow-Origin:"*",
+tmpl.innerHTML = `
+		<form id="form">
+		
+			<fieldset>
+				<legend>Gauge Box Properties</legend>
+				<table>
+					<tr>
+						<td>Value</td>
+						<td><input id="aps_val" type="text" name="colorProp" size="20" maxlength="20"></td>
+					</tr>
+				</table>
+			</fieldset>
+			<button type="submit">Submit</button>
+		</form>
+
+
+`;
+class GaugeAps extends HTMLElement {
+		  constructor() {
+		    super();
+		    this._shadowRoot = this.attachShadow({mode: 'open'});
+		    this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+		    this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
+		  }
+
+		  _submit(e) {
+		    	e.preventDefault();
+				this.dispatchEvent(new CustomEvent('propertiesChanged', { detail: { properties: {
+					colorProp: this.colorProp
+				}}}));
+				return false;
+		  }
+
+		  get colorProp() {
+			 return this._shadowRoot.getElementById("aps_val").value ;
+	      }
+
+		  set colorProp(value) {
+			  this._shadowRoot.getElementById("aps_val").value = value;
+		  }
+
+		 
+		  static get observedAttributes() {
+			  return ['colorProp'];
+	      }
+
+		  attributeChangedCallback(name, oldValue, newValue) {
+			 if (oldValue != newValue) {
+				  this[name] = newValue;
+			 }
+		  }
+}
+
+customElements.define('com-iprosis-sample-gauge-aps', GaugeAps);
+})();
---
+++ webcomponent.js
@@ -0,0 +1,49 @@
+(function()  {
+let tmpl = document.createElement('template');
+
+/* CSS within the project */
+tmpl.innerHTML = `
+  <style>
+  .coloredBox {
+	border-radius:25px;
+	border-width: 4px;
+	border-color: black;
+	border-style: solid;
+}
+  </style>
+ 
+`;
+
+class Gauge extends HTMLElement {
+
+	constructor() {
+		/*specify that we have a ShadowRoot and the cloned template needs to be appended to the Shadow DOM instead of directly to the DOM */
+		super();
+		/* Shadow DOM */
+		this._shadowRoot = this.attachShadow({mode: 'open'});
+		this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+
+		this.style.height = "100%";
+		this._shadowRoot.addEventListener("onClick", this.onclick());
+		this._urlLink = "https://www.walla.co.il";		
+		this.adjustCssClasses();
+	};
+
+	adjustCssClasses() {
+		this.className  = "coloredBox";
+	}
+
+    get urlLink() {
+    	return this._urlLink;
+    }
+    set urlLink(value) {
+		this._urlLink = value;
+	};
+
+	onclick() {
+		window.open(this._urlLink);
+  }
+};
+  /* Define web component - input: tag and class */
+  customElements.define('com-iprosis-sample-gauge', Gauge);
+})();
