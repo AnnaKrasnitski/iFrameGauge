7d0f19ba1580edca8430c9c6e82369e31efc6072
AnnaKrasnitski <53522877+AnnaKrasnitski@users.noreply.github.com>
2019-08-04 15:08:25 +0300

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -4,12 +4,6 @@ let tmpl = document.createElement('template');
 /* CSS within the project */
 tmpl.innerHTML = `
   <style>
-  .coloredBox {
-	border-radius:25px;
-	border-width: 4px;
-	border-color: black;
-	border-style: solid;
-}
   </style>
  
 `;
@@ -26,11 +20,11 @@ class Gauge extends HTMLElement {
 		this.style.height = "100%";
 		this._shadowRoot.addEventListener("onClick", this.onclick());
 		this._urlLink = "https://www.walla.co.il";		
-		this.adjustCssClasses();
+		//this.adjustCssClasses();
 	};
 
 	adjustCssClasses() {
-		this.className  = "coloredBox";
+		
 	}
 
     get urlLink() {
@@ -40,8 +34,8 @@ class Gauge extends HTMLElement {
 		this._urlLink = value;
 	};
 
-	onclick() {
-		window.open(this._urlLink);
+    onclick() {
+	window.open(this._urlLink);
   }
 };
   /* Define web component - input: tag and class */
