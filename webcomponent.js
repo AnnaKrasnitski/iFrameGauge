6ae4a39fdd891a48716955f2347b543fc3e3065c
AnnaKrasnitski <53522877+AnnaKrasnitski@users.noreply.github.com>
2019-08-06 10:12:16 +0300

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -1,9 +1,14 @@
+	(function()  {
+	let tmpl = document.createElement('template');
+	
 	/* CSS within the project */
 	tmpl.innerHTML = `
 
-		
-<a href="https://www.walla.co.il" target="theFrame"> </a><br /><br />
-<iframe id="frameid" width="500" height="500" src="https://www.walla.co.il" name="theFrame"></iframe>
+	  <div class="thumbnail-container">
+  		<div class="thumbnail">
+   		<iframe id="frameid" src= "https://www.walla.co.il" frameborder="0" width= 200 height= 200></iframe>
+  		</div>
+	</div>
 	 
 	`;
 	
@@ -16,21 +21,8 @@
 			this._shadowRoot = this.attachShadow({mode: 'open'});
 			this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
 			this.style.height = "100%";
-			//this._shadowRoot.getElementById("frameid").contentWindow.location.href;
-		
-			//this._shadowRoot.addEventListener("click", this.onClick());
-			 
-			//this._shadowRoot.addEventListener("click", event => {
-			//	var event = new Event("onClick");
-			//	this.dispatchEvent(event);
-			//	});
 			
 			 this.addEventListener("click", function() {
-				window.open(this._urlLink,"_blank");
-				console.log("window opened");
-			}, false);
-
-			document.getElementById("frameid").addEventListener("click", function() {
 				window.open(this._urlLink,"_blank");
 				console.log("window opened");
 			}, false);
@@ -48,13 +40,6 @@
 			this._urlLink = value;
 		}
 	
-	//     onClick() {
-	// 	//window.location.assign("https://www.sap.com/index.html");
-	// 	window.open(this._urlLink,"_blank");
-	// 	window.open("https://www.sap.com/index.html","_blank");
-	// 	console.log("window opened");
-	//   }
-
 	
 	}
 	  /* Define web component - input: tag and class */
