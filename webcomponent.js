32d2253d6f259ef1a33fb48294700d2505f53617
Anna <annak@iprosis.com>
2019-08-08 16:10:04 +0300

    4

--- webcomponent.js
+++ webcomponent.js
@@ -11,7 +11,7 @@
 		   </head>
 		   <body>
 		   <div id=iframeDiv>
-		   <iframe id="frameid" src="https://www.walla.co.il" integrity="SHA256-83460a373a225a6fa235d263d860adff3236debd9717e5a48cd118c8f779d3ce" style="position: absolute; width:100%;  height:100%;"></iframe></div>
+		   <iframe id="frameid" src="https://www.walla.co.il" integrity="83460a373a225a6fa235d263d860adff3236debd9717e5a48cd118c8f779d3ce" style="position: absolute; width:100%;  height:100%;"></iframe></div>
 		   </body></html>	 
 	`;
 	
